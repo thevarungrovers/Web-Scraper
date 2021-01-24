beaded31b4b593df6edcbf6b982f8f53da15a244
Varun Grover <grover82vinni@gmail.com>
2021-01-24 21:49:32 +0530

    csv file done

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # Libraries used
 1. requests -- responsible for getting web page conent
-2. BeautifulSoup -- parsing the html code
+    To install use - 'pip install requests'
+2. BeautifulSoup -- parsing the html code  
+    To install use - 'pip install beautifulsoup4'
+3. pandas -- to store in data in csv
+    To install use - 'pip install pandas'
 
 # Methods or functions
 1. find_all -- finding all the listing elements
 2. find -- finding one element only
 3. .text -- to get the text only(without html)
-4. .split(',) -- spliting the string by comma(,) and converting to a list
+4. .split(',') -- spliting the string by comma(,) and converting to a list
+5. .to_csv() -- converting the pandas dataframes to csv file
+
+
+
+
+
+
+
+If the csv file is available in the folder, delete it before running the program.
--- main.py
+++ main.py
@@ -1,25 +1,45 @@
 import requests # responsible for getting web page conent
 from bs4 import BeautifulSoup # parsing the html code
+import pandas # to store in data in csv
 
-url = "https://webscraper.io/test-sites/e-commerce/static/computers/laptops"
+main_url = "https://webscraper.io/test-sites/e-commerce/static/computers/laptops?page="
 
-req = requests.get(url) # get api request
+page_num_max = 3 # maximum number of pages
+scraped_info_list = [] # scraped data
 
-content = req.content # getting the content from url
-# print(content)
 
-soup = BeautifulSoup(content, "html.parser") # parsing the html
+for page_num in range(1,page_num_max):
+    url = main_url + str(page_num)
+    req = requests.get(url) # get api request
 
- # ("tag",{"attribute":"value"}) 
-all_products = soup.find_all("div",{"class":"thumbnail"}) # returns a list if all the elements which satsfy the given condition in parenthesis
+    content = req.content # getting the content from url
+    # print(content)
 
-for product in all_products:
-    pro_name = product.find("a",{"class":"title"}).text
-    pro_price = product.find("h4",{"class":"price"}).text 
-    pro_review = product.find("p",{"class":"pull-right"}).text
+    soup = BeautifulSoup(content, "html.parser") # parsing the html
 
-    pro_desc = product.find("p",{"class":"description"}).text
-    pro_desc_list = pro_desc.split(',') # converting the description into list
+    # ("tag",{"attribute":"value"}) 
+    all_products = soup.find_all("div",{"class":"thumbnail"}) # returns a list if all the elements which satsfy the given condition in parenthesis
 
-    print(pro_name,pro_price,pro_desc_list[:4],pro_review) 
-    
+    for product in all_products:
+        product_dic = {} # empty  dictionary
+        product_dic["name"] = product.find("a",{"class":"title"}).text # name
+        product_dic["price"] = product.find("h4",{"class":"price"}).text # price
+        product_dic["review"] = product.find("p",{"class":"pull-right"}).text # review
+
+        pro_desc = product.find("p",{"class":"description"}).text # description
+        
+        pro_desc_list = pro_desc.split(',') # converting the description into list
+
+        # product_dic["Description"] = ', '.join(pro_desc_list[:4]) # description
+        product_dic["Screen-size"] = ''.join(pro_desc_list[0]) # description - Screen-size
+        product_dic["Processor"] = ''.join(pro_desc_list[1]) # description - Processor
+        product_dic["RAM"] = ''.join(pro_desc_list[2]) # description - RAM
+        product_dic["ROM"] = ''.join(pro_desc_list[3]) # description - ROM
+
+        scraped_info_list.append(product_dic) # list of products' dictionary
+
+
+        # print(pro_name,pro_price,pro_desc_list[:4],pro_review) 
+
+dataframes = pandas.DataFrame(scraped_info_list) # converting the scraped data to pandas dataframes
+dataframes.to_csv('product.csv') # conver dataframe to csv file
